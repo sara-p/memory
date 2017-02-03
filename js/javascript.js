732524ab558c0a651502bd970126241dd3b21734
Sara Pettersson <sara_pettersson@dennis.co.uk>
2017-02-03 12:03:06 +0000

    create cards

--- js/javascript.js
+++ js/javascript.js
@@ -12,12 +12,34 @@
 //   - press play
 //   - shuffle cards
 
- 
+window.onload = init;
 
-var cards = document.getElementsByClassName('cards');
-for (var i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('click', flip);
+var colours = ['red', 'green', 'blue', 'black', 'pink', 'red', 'green', 'blue', 'black', 'pink', ];
+
+function init() {
+
+	// how many cards
+	var gameSize = 10;
+	
+	// create cards
+	for (var i = 0; i < gameSize; i++) {		
+		var li = document.createElement('li');
+		li.classList.add('cards');
+		li.innerHTML = '<div class="front">front</div><div class="back">'+ colours[i] + '</div>';
+		var board = document.getElementById('board');
+		// add cards on board
+		board.appendChild(li); 
+		
+		li.addEventListener('click', flip);
+	}
+
+	//event Listener for cards
+	// var cards = document.getElementsByClassName('cards');
+	// for (var i = 0; i < cards.length; i++) {
+	//   cards[i].addEventListener('click', flip);
+	// }
 }
+
 
 function flip() {
   if (this.classList.contains('flipped')) {
